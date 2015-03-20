3441e21609cfa0d2e8808ec316e0afb4df198aab
fridlutz <wilfrid.utz@gmail.com>
2015-03-20 11:42:59 +0100

    Update task log, update 1-3 result.

--- INTERROP/Session 1/Aufgabe1-3.sql
+++ INTERROP/Session 1/Aufgabe1-3.sql
@@ -1,2 +1,4 @@
+DROP TABLE teilnehmerxml;
 CREATE TABLE teilnehmerxml (TNNr INTEGER, Content XML); 
-INSERT INTO TEILNEHMERXML VALUES(215, '<Teilnehmer><Name>Gerstner, M.</Name><Ort>Ulm</Ort></Teilnehmer>');
+INSERT INTO TEILNEHMERXML VALUES(215, '<Teilnehmer><Name>Gerstner, M.</Name><Ort>Ulm</Ort></Teilnehmer>');;
+SELECT * FROM teilnehmerxml;
