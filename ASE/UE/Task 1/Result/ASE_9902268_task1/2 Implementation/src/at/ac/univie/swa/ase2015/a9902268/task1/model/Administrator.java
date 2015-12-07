61ad388d1b78d8ca3fd3242c3e2fa9cb239d79fd
Wilfrid Utz <wilfrid@utz.co.at>
2015-12-07 13:48:01 +0100

    Deleted unarchived results

--- ASE/UE/Task 1/Result/ASE_9902268_task1/2 Implementation/src/at/ac/univie/swa/ase2015/a9902268/task1/model/Administrator.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author Wilfrid Utz, a9902268, wilfrid.utz@gmail.com
- * */
-package at.ac.univie.swa.ase2015.a9902268.task1.model;
-
-public class Administrator extends AutoConfirmedUser {
-
-}
--- ASE/UE/Task 1/Result/ASE_9902268_task1/2 Implementation/src/at/ac/univie/swa/ase2015/a9902268/task1/model/AutoConfirmedUser.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Wilfrid Utz, a9902268, wilfrid.utz@gmail.com
- * */
-package at.ac.univie.swa.ase2015.a9902268.task1.model;
-
-public class AutoConfirmedUser extends RegisterUser {
-	
-
-}
